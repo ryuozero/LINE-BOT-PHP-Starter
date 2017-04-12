54ae280182e7e33e973adbff33217eda0c84a5b8
ryuozero <junta753@hotmail.com>
2017-04-12 11:17:08 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -26,12 +26,13 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-}else{
+}
+/*else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
-}
+}*/
  
  
 $ch = curl_init();
