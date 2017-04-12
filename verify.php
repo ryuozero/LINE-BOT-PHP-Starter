89eaf866566bd42ee80456078b913106fed82756
ryuozero <junta753@hotmail.com>
2017-04-12 08:32:26 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,10 +1,15 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
+$access_token = 'mmVUjVCOox5K+ZzBq/tyA3HAX6XKng5kjjRiHxhcib2vNJ1jdqRWSI4Fatg00DhJpN1v4pNVQkzgj+rbFqh2A0TV8rymXuXy5R24BZAN3c7zDBCObVbdF2ZfHGiJXme8SAjoaowamn349PcXNKI70gdB04t89/1O/w1cDnyilFU=';
 
-<body>
-</body>
-</html>
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
